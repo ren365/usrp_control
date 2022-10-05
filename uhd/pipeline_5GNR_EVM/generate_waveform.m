23a9455e085c70e7712721098525b0b8592864e7
zehao <zehao.w@duke.edu>
2022-10-05 12:59:49 -0400

    change padding

--- uhd/pipeline_5GNR_EVM/generate_waveform.m
+++ uhd/pipeline_5GNR_EVM/generate_waveform.m
@@ -1,4 +1,4 @@
 zeropadding = zeros(1e5,1);
 waveform = waveStruct.waveform;
 waveform_new = [zeropadding;waveform;zeropadding];
-Wave2File("5GNR_sent_withPadding.bin",waveform_new*18);
+Wave2File("5GNR_sent_withPadding.bin",waveform_new*15);
