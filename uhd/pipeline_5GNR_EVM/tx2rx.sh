aac656a661be62b8259332a78712cebd1ef2116d
Zehao Wang <zw246@tc319-srv2.egr.duke.edu>
2022-12-11 17:16:53 -0500

    small changes

--- uhd/pipeline_5GNR_EVM/tx2rx.sh
+++ uhd/pipeline_5GNR_EVM/tx2rx.sh
@@ -14,7 +14,7 @@ bandRx=50000000
 rateTx=$bandTx
 rateRx=$bandRx
 gain=10
-freq=3000000000
+freq=1000000000
 fileTx="tx.bin"
 fileRx="rx.bin"
 
@@ -23,5 +23,5 @@ fileRx="rx.bin"
 ./tx_samples_from_file --args serial=$serialTx --channel $channelTx --ant $antTx --gain $gain --file $fileTx --type float --freq $freq --rate $rateTx --bw $bandTx --repeat &
 txpid=$!
 sleep 7s
-./rx_samples_to_file --args serial=$serialRx --channel $channelRx --ant $antRx --gain $gain --file $fileRx --type float --freq $freq --rate $rateRx --bw $bandRx --nsamps 300000
+./rx_samples_to_file --args serial=$serialRx --channel $channelRx --ant $antRx --gain $gain --file $fileRx --type float --freq $freq --rate $rateRx --bw $bandRx --nsamps 3000000
 kill -9 $txpid
